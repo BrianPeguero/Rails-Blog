f097e0735f5718349bdf2e5a4201d7979fd10c52
Brian Peguero <brianpeguero@gmail.com>
2018-08-11 18:03:23 -0400

    Added an authentication method for deleting comments and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "Brian", password: "0323", except: [:index, :show]
+    
     def new
         @article = Article.new
     end
